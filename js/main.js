f4b5e0b2241536560372de43dda08a9f73168c6f
Abay <ibrayev.abay@gmail.com>
2023-09-29 14:34:23 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-// Create function to track turns
-// Create dropping colored token function - color determined by turn/player
+/*---- State Variables  ----*//*
+    player turns: 1/-1
+    board: 2D array
+        null(0) -> no player at cell / empty
+        1/-1    -> player at cell
+    winner:
+        null(0) -> no winner
+        1/-1    -> winner
+        'T'     -> tie
+*/
+/*---- Functions        ----*//*
+    Render
+        board
+        tokens
+        turns
+        available options
+*/
 
+/*---- constants        ----*/
+/*---- state variables  ----*/
+/*---- cached elements  ----*/
+/*---- event listeners  ----*/
+/*---- functions        ----*/
